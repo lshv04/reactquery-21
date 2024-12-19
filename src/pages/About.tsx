917f51d525a77e750f6f5b3644b0b25fc581da91
Leandro Hosken <lshv04@gmail.com>
2024-12-19 11:20:00 -0300

    trabalhando about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,23 @@
-// src/pages/About.tsx
-
 import React from "react";
-import styles from "./About.module.css"; // Importando o CSS modular
+import styles from "./About.module.css";
+import InputBar from "../components/InputBar";
 
 const About: React.FC = () => {
   return (
     <div className={`container-fluid bord ${styles.about}`}>
       <div className="container bord">
-      <h1 className={styles.title}>About Us</h1>
-      <p className={styles.description}>
-        This project was developed by Leandro Hosken with the goal of learning
-        and improving skills in Axios API fetching, as well as in React Query
-        caching.
-      </p>
-    </div>
+        <div className="row">
+          <div className="col-12 col-md-4 bord">
+            <h1 className={styles.title}>About Us</h1>
+            <p className={styles.description}>
+              This project was developed by Leandro Hosken with the goal of
+              learning and improving skills in Axios API fetching, as well as in
+              React Query caching.
+            </p>
+            <InputBar/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
