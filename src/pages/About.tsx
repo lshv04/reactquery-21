cd693bdf7bf3cbf5035cf65256d33fc8d09aa9a7
Leandro Hosken <lshv04@gmail.com>
2024-12-19 11:00:31 -0300

    trabalhando about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,19 @@
 // src/pages/About.tsx
 
-import React from 'react';
-import styles from './About.module.css'; // Importando o CSS modular
+import React from "react";
+import styles from "./About.module.css"; // Importando o CSS modular
 
 const About: React.FC = () => {
   return (
-    <div className={styles.about}>
+    <div className={`container-fluid bord ${styles.about}`}>
+      <div className="container bord">
       <h1 className={styles.title}>About Us</h1>
       <p className={styles.description}>
-        We are a company dedicated to providing the best flight search experience.
+        This project was developed by Leandro Hosken with the goal of learning
+        and improving skills in Axios API fetching, as well as in React Query
+        caching.
       </p>
-      <p className={styles.description}>
-        Our goal is to help you find the most affordable flights and travel easily.
-      </p>
+    </div>
     </div>
   );
 };
