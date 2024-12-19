c7f9128045f8ab2595a35bfde9cf08247c5208d2
Leandro Hosken <lshv04@gmail.com>
2024-12-19 10:36:44 -0300

    trabalhando variaveis de cores e componente sticker

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import styles from './Footer.module.css'; // Importando o CSS modular
+import Sticker from './Sticker';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
+          <Sticker/>
         <p className={styles.text}>© 2024 Leandro Hosken. All rights reserved.</p>
         <div className={styles.links}>
           <a href="/about" className={styles.link}>About</a>
